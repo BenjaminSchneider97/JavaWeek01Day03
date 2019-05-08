674eaf8b56050c2b0ffe260bef194c94460c08eb
Simon Blaha <s.blaha@live.at>
2019-05-08 10:14:08 +0200

    add: basic exercise 1

---
+++ src/Basic_A1.java
@@ -0,0 +1,11 @@
+public class Basic_A1 {
+
+    public static void main(String[] args) {
+
+        String benjamin = "Did Benjamin see bees? Benjamin did.";
+
+       int a = benjamin.length();
+
+       System.out.println(a);
+    }
+}
