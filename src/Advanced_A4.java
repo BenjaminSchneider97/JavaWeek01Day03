25350162e6b563ca119407fb102fe6629b973297
Simon Blaha <s.blaha@live.at>
2019-05-08 11:05:59 +0200

    create: exercise 4

---
+++ src/Advanced_A4.java
@@ -0,0 +1,12 @@
+public class Advanced_A4 {
+
+    public static void main(String[] args) {
+
+    String name;
+    System.out.println("Enter your name ");
+    Scanner input = new Scanner(System.in);
+    name = input.nextLine();
+
+    
+    }
+}
