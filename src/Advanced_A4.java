e88b7df932eaeb6f7d8290c2bfc1cb020ae047ba
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-08 11:21:30 +0200

    finish advanced a4

--- src/Advanced_A4.java
+++ src/Advanced_A4.java
@@ -1,12 +1,33 @@
+import java.util.Scanner;
+
 public class Advanced_A4 {
 
     public static void main(String[] args) {
 
-    String name;
-    System.out.println("Enter your name ");
-    Scanner input = new Scanner(System.in);
-    name = input.nextLine();
+        /*
+        Scanner input = new Scanner(System.in);
 
-    
+        System.out.println("Enter your first name: ");
+        String fname = input.nextLine();
+
+        System.out.println("Enter your last name: ");
+        String lname = input.nextLine();
+
+        String sfname = fname.substring(0, 1);
+        String slname = lname.substring(0, 1);
+
+        System.out.println("Hello " + sfname + "." + slname + ".");
+
+        */
+
+        Scanner name = new Scanner(System.in);
+
+        System.out.print("Enter you name: ");
+        String fullname = name.nextLine();
+
+        String[] splitname = fullname.split(" ");
+
+        System.out.println("Hello " + splitname[0].substring(0, 1) + "." + splitname[1].substring(0, 1) + ".");
+
     }
 }
