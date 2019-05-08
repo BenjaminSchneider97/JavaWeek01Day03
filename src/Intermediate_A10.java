e4a9c42b291bee330ca21712ae3ea515c7b1751d
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-08 14:11:43 +0200

    add and finish a10

---
+++ src/Intermediate_A10.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Intermediate_A10 {
+
+    public static String calcAvg(){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int a = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = input.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int c = input.nextInt();
+
+        int avg = (a+b+c)/3;
+
+        String result = "The average of " + a + ", " + b + " and " + c + " is: " + avg;
+
+        return result;
+
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(calcAvg());
+
+    }
+
+}
