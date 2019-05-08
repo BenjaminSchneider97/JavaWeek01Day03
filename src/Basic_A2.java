64175668bb047c874fb32af260dc1582972c1636
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-08 10:33:25 +0200

    Add Basic A2

---
+++ src/Basic_A2.java
@@ -0,0 +1,12 @@
+public class Basic_A2 {
+
+    public static void main(String[] args) {
+
+        String a = "Was it a car or a cat I saw?".substring(9, 12);
+
+        int b = a.length();
+
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
