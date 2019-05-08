e25ff9a7064a49218459a59a772486aa276a1e50
Simon Blaha <s.blaha@live.at>
2019-05-08 10:27:00 +0200

    finish: basic exercise 1

--- src/Basic_A1.java
+++ src/Basic_A1.java
@@ -7,5 +7,13 @@ public class Basic_A1 {
        int a = benjamin.length();
 
        System.out.println(a);
+
+       char b = benjamin.charAt(15);
+
+       System.out.println(b);
+
+       char c = benjamin.charAt(17);
+
+       System.out.println(c);
     }
 }
